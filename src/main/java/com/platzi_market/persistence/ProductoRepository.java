ae8410e404bb16c8742e65e5b98ad3e720dfb181
Camilo Valdes <juan.valdes@newsoftlatam.com>
2025-10-29 16:27:49 -0500

    Consultas avanzadas con Query Methods en Spring data

--- src/main/java/com/platzi_market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi_market/persistence/ProductoRepository.java
@@ -2,7 +2,10 @@ package com.platzi_market.persistence;
 
 import com.platzi_market.persistence.crud.ProductoCrudRepository;
 import com.platzi_market.persistence.entity.Producto;
+import org.hibernate.mapping.Value;
+
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
@@ -11,4 +14,11 @@ public class ProductoRepository {
         return (List<Producto>)productoCrudRepository.findAll();
     }
 
+    public List<Producto> GetByIdCategoria(int idCategoria) {
+        return (List<Producto>)productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> GetEscasos(int cantidad, boolean estado) {
+        return (Optional<List<Producto>>)productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, estado);
+    }
 }
--- src/main/java/com/platzi_market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi_market/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,14 @@
 package com.platzi_market.persistence.crud;
 
+import com.platzi_market.persistence.entity.Categoria;
 import com.platzi_market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
