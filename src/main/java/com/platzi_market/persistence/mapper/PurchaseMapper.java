a22eed4db5cc2e77365715e51b513279a08d21fa
Camilo Valdes <juan.valdes@newsoftlatam.com>
2025-11-01 00:46:36 -0500

    ajustando bug de creación de ordenes

--- src/main/java/com/platzi_market/persistence/mapper/PurchaseMapper.java
+++ src/main/java/com/platzi_market/persistence/mapper/PurchaseMapper.java
@@ -25,5 +25,6 @@ public interface PurchaseMapper {
 
     @InheritInverseConfiguration
     @Mapping(target = "cliente", ignore = true)
+    @Mapping(target = "idCompra", ignore = true)
     Compra toCompra(Purchase purchase);
 }
