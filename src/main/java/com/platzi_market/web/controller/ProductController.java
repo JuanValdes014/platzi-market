a62d98e6639edc4659fc02784bac81a80b2b78fc
Camilo Valdes <juan.valdes@newsoftlatam.com>
2025-11-01 01:23:17 -0500

    generando documentación en api con swagger

--- src/main/java/com/platzi_market/web/controller/ProductController.java
+++ src/main/java/com/platzi_market/web/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.platzi_market.web.controller;
 
 import com.platzi_market.domain.Product;
 import com.platzi_market.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,20 +17,39 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    @Autowired
     private ProductService productService;
 
-    @Autowired
-    public void setProductService(ProductService productService) {
-        this.productService = productService;
-    }
-
     @GetMapping("/all")
+    @Operation(
+            summary = "Get all supermarket products",
+            description = "Retrieve the list of all available supermarket products",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful retrieval"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     public ResponseEntity getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    @Operation(
+            summary = "Search a product with an ID",
+            description = "Retrieve the result of one available supermarket product",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful retrieval"),
+                    @ApiResponse(responseCode = "404", description = "Product not found")
+            }
+    )
+    public ResponseEntity<Product> getProduct(
+        @Parameter(
+                name = "id",
+                description = "Unique identifier of the product",
+                required = true,
+                example = "25"
+        )
+        @PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
